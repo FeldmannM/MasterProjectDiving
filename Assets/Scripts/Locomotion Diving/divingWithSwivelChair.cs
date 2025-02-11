88a8d2d181ae9f691877714d639a5b8f8bab5a3c
FeldmannM <s4mifeld@web.de>
2025-02-11 18:20:58 +0100

    some improvements

---
+++ Assets/Scripts/Locomotion Diving/divingWithSwivelChair.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class divingWithSwivelChair : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject locomotion;
+    [SerializeField]
+    private Camera mainCamera;
+	[SerializeField]
+    private float maxSpeed = 1.5f;
+	[SerializeField]
+	private float acceleration = 5f;
+	[SerializeField]
+	private float deceleration = 0.75f;
+    [SerializeField]
+    private float movementThreshold = 0.004f;
+    [SerializeField]
+    private InputActionReference leftConPos;
+    [SerializeField]
+    private InputActionReference rightConPos;
+
+    private Vibration vibration;
+
+    private Vector3 lastLeftConPos;
+    private Vector3 lastRightConPos;
+    private Vector3 currentLeftConPos;
+    private Vector3 currentRightConPos;
+	private float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (leftConPos != null && rightConPos != null)
+        {
+            lastLeftConPos = leftConPos.action.ReadValue<Vector3>();
+            lastRightConPos = rightConPos.action.ReadValue<Vector3>();
+            vibration = locomotion.GetComponent<Vibration>();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(leftConPos != null && rightConPos != null)
+        {
+            currentLeftConPos = leftConPos.action.ReadValue<Vector3>();
+            currentRightConPos = rightConPos.action.ReadValue<Vector3>();
+
+            Vector3 movementLeft = currentLeftConPos - lastLeftConPos;
+            Vector3 movementRight = currentRightConPos - lastRightConPos;
+
+            Vector3 forwardDirection = mainCamera.transform.forward;
+            float movementMagnitudeLeft = Vector3.Dot(movementLeft, forwardDirection);
+            float movementMagnitudeRight = Vector3.Dot(movementRight, forwardDirection);
+
+            if (movementMagnitudeLeft < - movementThreshold && movementMagnitudeRight < - movementThreshold)
+            {
+				currentSpeed += acceleration * Time.deltaTime;
+				currentSpeed = Mathf.Clamp(currentSpeed, 0f, maxSpeed);
+				
+                Vector3 movement = forwardDirection * currentSpeed * Time.deltaTime;
+                locomotion.transform.position += movement;
+            }
+			else{
+				currentSpeed -= deceleration * Time.deltaTime;
+				currentSpeed = Mathf.Max(currentSpeed, 0f);
+				
+                Vector3 movement = forwardDirection * currentSpeed * Time.deltaTime;
+                locomotion.transform.position += movement;
+			}	
+			
+            lastLeftConPos = currentLeftConPos;
+            lastRightConPos = currentRightConPos;
+            Debug.Log(currentSpeed);
+
+            float bIntensity = (currentSpeed / maxSpeed) * 0.25f;
+            if (bIntensity > 0.025f)
+            {
+                vibration.activeVib = true;
+                vibration.setAmplitude(bIntensity);
+            }
+            else
+            {
+                vibration.activeVib = false;
+            }
+        }
+    }
+}
